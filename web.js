8ec8e4740ad190aaf7f038fc891bb58d9342b981
John.Smith <example@standford.edu>
2013-07-16 08:41:14 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
-
+var fs =require('fs');
 var app = express.createServer(express.logger());
+var infile = "index.html";
+var data = new Buffer(14);
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+    fs.readFileSync(infile,function(err,data));
+    
+   response.send(data.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
