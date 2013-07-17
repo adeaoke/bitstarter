6f053b9bee44c88d4451781496f2a0de35c81f4b
John.Smith <example@standford.edu>
2013-07-17 06:24:46 +0000

    Add hello index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var fs =require('fs');
 var infile = "index.html";
-var data = new Buffer(50);
+var data = new Buffer(1024);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    data = fs.readFileSync(infile);
-    response.send(data.toString('utf8',0,50));
+    
+    var i=1;
+    for(i=1; i< 2; i++) {
+	data = fs.readFileSync(infile);
+	response.send(data.toString('utf8',0,1024));
+    }
 });
 
 var port = process.env.PORT || 5000;
