5c56d40ae53a328c5373a43db0caca43b47ef45c
John.Smith <example@standford.edu>
2013-07-17 23:14:56 +0000

    add new items

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var fs =require('fs');
 var infile = "index.html";
-var data = new Buffer(1024);
+var content;
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     
-    var i=1;
-    for(i=1; i< 103; i++) {
-	data = fs.readFileSync(infile);
-	response.send(data.toString('utf8',0,1024));
-    }
+    fs.readFile('./Index.html', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        content = data;
+    });
+    response.send(data.toString('utf8')); // console.log(content);
 });
 
 var port = process.env.PORT || 5000;
@@ -21,3 +23,11 @@ app.listen(port, function() {
 
 
 
+var content;
+    fs.readFile('./Index.html', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        content = data;
+    });
+    console.log(content);
