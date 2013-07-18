e854535f66ec407613f3ff140a83e3e287a15499
John.Smith <example@standford.edu>
2013-07-18 06:24:51 +0000

    update web.js version 1

--- web.js
+++ web.js
@@ -1,32 +1,19 @@
 var express = require('express');
 var fs =require('fs');
-
+var infile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     
-    fs.readFile('./index.html', 'utf8', function read(err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        response.send(data);
+    fs.readFile(infile, 'utf8', function read(err, data) {
+        if (err) { return console.log(err);}
+	response.send(data);
   });
-
 });
 
+
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
-
-
-var content;
-    fs.readFile('./Index.html', function read(err, data) {
-        if (err) {
-            throw err;
-        }
-        content = data;
-    });
-    console.log(content);
